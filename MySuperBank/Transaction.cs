94d7f368107ef893fc833b2b0e6b9e525106f4c3
rahulvasi <vasireddi.rahul@gmail.com>
2020-06-27 23:11:19 +0530

    CSharp Project

---
+++ MySuperBank/Transaction.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+
+namespace MySuperBank
+{
+    public class Transaction
+    {
+        public decimal Amount { get; }
+        public DateTime Date { get; }
+        public string Notes { get; }
+
+        public Transaction(decimal amount, DateTime date, string note)
+        {
+            this.Amount = amount;
+            this.Date = date;
+            this.Notes = note;
+        }
+    }
+}
